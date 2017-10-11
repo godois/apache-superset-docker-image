2a68cdbe5d14ae599d25f5f95e801bbd6328c501
Marcio Godoi <souzagodoi@gmail.com>
2017-10-11 19:33:07 -0300

    Adding initialization commands to this file, removing from the Dockerfile

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
+if [ "$NEWINSTANCE" = true ];
+then
+    echo "Superset is creating an admin user... "
+	fabmanager create-admin --app superset --username adm --password 123 --firstname Admin --lastname "" --email admin@example.com
+
+    echo "Superset is upgrading databases... "
+    superset db upgrade
+
+    echo "Superset is loading examples... "
+    superset load_examples
+
+    echo "Superset init()... "
+    superset init
+	
+fi
+echo "Superset is starting the server... "
 superset runserver
+
+
+
